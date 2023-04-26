51a55109f9f6b0caa5d530b3239840cff21509c5
Divya <beingdivyadixit@gmail.com>
2023-04-26 07:01:09 +0530

    UC1_CountDuplicateNumbers

---
+++ PracticeProblems_4B/ConutDuplicatevalues.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticeProblems_4B
+{
+    class ConutDuplicatevalues
+    {
+        public static void arrayInfo()
+        {
+            int[] a = new int[5];
+            int i, j, count = 0;
+            Console.WriteLine("Elements");
+            for (i = 0; i < a.Length; i++)
+            {
+                Console.WriteLine("Enter Elements");
+                a[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            Console.WriteLine("array Elements");
+            for (i = 0; i < a.Length; i++)
+            {
+                Console.WriteLine("Enter Elements" + a[i]);
+            }
+            for (i = 0; i < a.Length; i++)
+            {
+                for (j = i + 1; j < a.Length; j++)
+                {
+                    if (a[i] == a[j])
+                    {
+                        count++;
+                        Console.WriteLine(a[i]);
+                        break;
+                    }
+                }
+            }
+            Console.WriteLine("Number of Elements repeated" + count);
+        }
+    }
+}
